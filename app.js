941c8d95457789d074993aea48ce775717899159
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-06-01 18:07:24 +0530

    add models

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+module.exports = app;
---
+++ config/server.js
@@ -0,0 +1,5 @@
+const app = require("../app");
+
+app.listen(3000, () => {
+  console.log("working on the port 3000");
+});
